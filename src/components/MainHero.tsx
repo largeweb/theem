dafa6a6aa364bc9dd2fbfad151f9469bf44720b8
matt <matthewgordon64@gmail.com>
2024-01-04 18:31:37 -0500

    logo and text changes

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -10,9 +10,20 @@ const MainHero = () => {
         <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
           <span className="block xl:inline">{mainHero.title}</span>{' '}
           <br className="hidden lg:inline" />
-          <span className={`block text-primary xl:inline`}>
-            {mainHero.subtitle}
-          </span>
+          <div style={{position: 'relative'}}>
+            <span className={`block xl:inline`} style={{position: 'absolute', left: '5px', top: '0px', color:"rgb(255, 0, 255)"}}>
+              {mainHero.subtitle}
+            </span>
+            <span className={`block xl:inline`} style={{position: 'absolute', left: '10px', top: '0px', color:"black"}}>
+              {mainHero.subtitle}
+            </span>
+            <span className={`block xl:inline`} style={{position: 'absolute', left: '15px', top: '0px', color:"cyan"}}>
+              {mainHero.subtitle}
+            </span>
+            <span className={`block xl:inline`} style={{position: 'relative', color:"transparent"}}>
+              {mainHero.subtitle}
+            </span>
+          </div>
         </h1>
         <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
           {mainHero.description}
